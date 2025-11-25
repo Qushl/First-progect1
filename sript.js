2707ded675650466c91d42c580fbea767b654ca8
Roman <vipioyt@mail.ru>
2025-11-25 20:16:06 +0500

    Update sript.js

--- sript.js
+++ sript.js
@@ -3,3 +3,11 @@ const data = [];
 for (let i = 0; i < 2; i++) {
   console.log('1');
 }
+let jackSparrow = 'Джек Воробей';
+let captain = jackSparrow;
+
+console.log(`Одна переменная: ${jackSparrow}, вторая переменная: ${captain}`);
+
+captain= `Капитан ${jackSparrow}`;
+
+console.log(`Одна переменная: ${jackSparrow}, вторая переменная: ${captain}`);
