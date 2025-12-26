654f10f27ba9ce9c0257404c1f1e343e5fbdc572
Roman <vipioyt@mail.ru>
2025-12-26 15:22:14 +0500

    Update sript.js

--- sript.js
+++ sript.js
@@ -59,6 +59,10 @@ for (let i = 0; i < 2; i++) {
   console.log('13');
 }
 
+for (let i = 0; i < 2; i++) {
+  console.log('13');
+}
+
 const jackSparrow = 'Джек Воробей';
 let captain = jackSparrow;
 
